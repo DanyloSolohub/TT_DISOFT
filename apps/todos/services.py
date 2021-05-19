9545cceaa68778e03334497c6749d4e790d76125
DanyloSolohub <73057792+DanyloSolohub@users.noreply.github.com>
2021-05-19 21:50:25 +0300

    Update services.py

--- apps/todos/services.py
+++ apps/todos/services.py
@@ -6,3 +6,22 @@ class Services:
     def send_mail(subject, body, to, **kwargs):
         message = mail.EmailMessage(subject, body, to=to, **kwargs)
         message.send()
+'''
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class NotificationPagination(PageNumberPagination):
+    page_size = 20
+    max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data
+        })
+'''
